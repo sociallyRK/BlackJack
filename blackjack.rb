55ba05431cc250c97373e3937dfc111549aa8e59
RahulKhanna <sociallyrk@gmail.com>
2014-04-04 16:32:21 -0700

    blackjack v2

--- blackjack.rb
+++ blackjack.rb
@@ -17,46 +17,57 @@ class Blackjack
 						12=>"Q"
 					}
 
-		@hash_number = { 0=>10, 
-						1=>11,
-						2=>2,
-						3=>3,
-						4=>4,
-						5=>5,
-						6=>6,
-						7=>7,
-						8=>8,
-						9=>9,
-						10=>10,
-						11=>10,
-						12=>10
+		@hash_value = { "K"=>10, 
+						"A"=>11,
+						"2"=>2,
+						"3"=>3,
+						"4"=>4,
+						"5"=>5,
+						"6"=>6,
+						"7"=>7,
+						"8"=>8,
+						"9"=>9,
+						"10"=>10,
+						"J"=>10,
+						"Q"=>10
 					}
 	end
 
 	def play_game
 		p hand = self.deal()
+		while sum_hand(hand) < 21 do
+		
 		response = self.wanna_hit()
 			if response == "Yes" 
 			hit_card = self.hit()
 			hand.push hit_card
 			else
+			return puts "Your Final And Is #{hand}"			
 			end
 		p hand
 		puts "sum is #{sum_hand(hand)}"
-
+		end
+		if sum_hand(hand) > 21
+			puts "You Busted. Game Over"
+			Process.exit
+			else
+			puts "You got 21. You Won."
+		end
 	end
 	
 	def sum_hand(hand)
 		sum = 0
-		hand.each { |x| sum+=x }
+		hand.each { |x| sum+=@hash_value[x] }
 		return sum
 	end
 	def deal()
 		card1 = rand(12)
 		card2 = rand(12)
+		value_card1 = @hash_display[card1]
+		value_card2 = @hash_display[card2]
 		your_hand =[]
-		your_hand.push card1
-		your_hand.push card2
+		your_hand.push value_card1
+		your_hand.push value_card2
 		return your_hand
 	end
 
@@ -67,7 +78,7 @@ class Blackjack
 
 	def hit()
 		hitcard = rand(12)
-		#self.push hitcard
+		value_hitcard = @hash_display[hitcard]
 	end
 end
 
